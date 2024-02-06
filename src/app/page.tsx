05d4b8db918a8afa6c42726b28261d1f471ad207
EduardoSimoess <eduardorsimoes97@gmail.com>
2024-02-06 14:03:23 -0300

    :sparkles: feat: skills component with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Experience from "@/components/experience";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
           <Experience/>
+          <Skills/>
       </div>
     </main>
   );
